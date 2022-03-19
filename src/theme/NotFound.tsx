19552872f484a1e96557b7dd9f4713d65d015711
bsuth <bsuth701@gmail.com>
2022-03-19 21:02:28 +0900

    swizzle not found

---
+++ src/theme/NotFound.tsx
@@ -0,0 +1,8 @@
+import Layout from '@theme/Layout';
+import styles from './NotFound.module.scss';
+
+export default () => (
+  <Layout pageClassName={styles.notFound} title={'404'}>
+    404
+  </Layout>
+);
