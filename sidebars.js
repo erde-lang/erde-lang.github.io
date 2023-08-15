a05d2b27d7ad60ac4279342babfbaa7db3595766
bsuth <bsuth701@gmail.com>
2023-08-15 20:41:26 +0900

    refactor

--- sidebars.js
+++ sidebars.js
@@ -1,12 +1,12 @@
 module.exports = {
   sidebar: [
-    { type: 'doc', id: 'introduction' },
-    { type: 'doc', id: 'getting-started' },
-    { type: 'doc', id: 'reference' },
-    { type: 'doc', id: 'breaking-changes-lua' },
-    { type: 'doc', id: 'error-rewriting' },
-    { type: 'doc', id: 'api' },
-    { type: 'doc', id: 'playground' },
-    { type: 'doc', id: 'changelog' },
+    { id: 'index', type: 'doc' },
+    { id: 'getting-started', type: 'doc' },
+    { id: 'reference', type: 'doc' },
+    { id: 'breaking-changes-lua', type: 'doc' },
+    { id: 'error-rewriting', type: 'doc' },
+    { id: 'api', type: 'doc' },
+    { id: 'playground', type: 'doc' },
+    { id: 'changelog', type: 'doc' },
   ],
 };
